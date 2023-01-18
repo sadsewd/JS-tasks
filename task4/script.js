314ac6954ee1b64928d42e3f483d88079a95370f
Karlis <lacitiskarlis@gmail.com>
2023-01-18 13:48:27 +0200

    Completed task 4

---
+++ task4/script.js
@@ -0,0 +1,5 @@
+const link = document.getElementById('w3r');
+
+function getAttributes(){
+    console.log("href : " + link.href + "\nhreflang : " + link.hreflang + "\nrel : " + link.rel + "\ntarget : " + link.target + "\ntype : " + link.type);
+}
