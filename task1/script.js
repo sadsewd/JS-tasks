527e3eba3f0935399d33ec8aaa0d5a489e4b36fa
Karlis <lacitiskarlis@gmail.com>
2023-01-18 12:36:08 +0200

    Completed task 1

---
+++ task1/script.js
@@ -0,0 +1,8 @@
+var textEl = document.getElementById("text").style;
+
+function js_style(){
+    textEl.color = "red";
+    textEl.textAlign = "center";
+    textEl.fontWeight = "bold";
+    textEl.fontSize = "2rem";
+}
