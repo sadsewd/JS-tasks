e274da955c52d88dbb2115964bba312227e22f16
Karlis <lacitiskarlis@gmail.com>
2023-01-20 17:40:34 +0200

    Completed task 12

--- task12/script.js
+++ task12/script.js
@@ -1,1 +1,13 @@
-let paragraph = document.getElementsByTagName("p")[0];
+let boldText = document.getElementsByTagName("strong");
+
+function highlight(){
+    for(i = 0; i < boldText.length; i++){
+        boldText[i].style.textDecoration = "underline";
+}
+}
+
+function stopHighlighting(){
+    for(k = 0; k < boldText.length; k++){
+        boldText[k].style.textDecoration = "none";
+    }
+}
