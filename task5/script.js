a0ca8cae7af699a2f3be417285df34d6cd35f2d0
Karlis <lacitiskarlis@gmail.com>
2023-01-20 16:23:43 +0200

    Improved task 5, replaced concatination with template literals

--- task5/script.js
+++ task5/script.js
@@ -2,6 +2,6 @@ const table = document.querySelector("#sampleTable tbody");
 let counter = 3;
 
 function insert_Row(){
-    table.innerHTML += '<tr><td>Row'+counter+' cell1</td> <td>Row'+counter+' cell2</td></tr>';
+    table.innerHTML += `<tr><td>Row${counter} cell1</td> <td>Row${counter} cell2</td></tr>`;
     counter ++;
 }
