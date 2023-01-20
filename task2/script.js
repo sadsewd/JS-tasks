400225e8e8add0477120db4b962e44bcc444ee5a
Karlis <lacitiskarlis@gmail.com>
2023-01-20 16:17:41 +0200

    Fixed task 2, form refreshing after submiting it

--- task2/script.js
+++ task2/script.js
@@ -1,4 +1,8 @@
-
-function getFormvalue(){
+let form = document.getElementById("form1");
+function getFormValue(event){
+    event.preventDefault();
     console.log("First name : " + document.getElementById('fname').value + "\nLast name : " + document.getElementById('lname').value);
+    event.preventDefault();
 }
+
+form.addEventListener('submit', getFormValue);
